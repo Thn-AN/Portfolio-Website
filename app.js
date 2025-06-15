f7ad873bbd883758781f7876569f42ba2dcfbdcc
Thien-An <tnuualuu@gmail.com>
2025-06-16 03:43:28 +1000

    completed sidebar

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-const toggleButton = document.getElementById("toggle-btn")
-const sidebar = document.getElementById("sidebar")
+// Fix toggleSubMenu arrow interaction and rotate
+const toggleButton = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
 
-function toggleSidebar(){
-    sidebar.classList.toggle("close")
-    toggleButton.classList.toggle("rotate")
-    sidebar.getElementsByClassName("show")
-
-    closeAllSubmenus()
+function toggleSidebar() {
+    sidebar.classList.toggle("close");
+    toggleButton.classList.toggle("rotate");
+    closeAllSubmenus();
 }
 
-function toggleSubMenu(button){
-    if(!button.nextElementSibling.classList.contains("show")){
-        closeAllSubmenus()
+function toggleSubMenu(button) {
+    const submenu = button.closest("li").querySelector(".sub-menu");
+    if (!submenu.classList.contains("show")) {
+        closeAllSubmenus();
     }
-    button.nextElementSibling.classList.toggle("show")
-    button.classList.toggle("rotate")
-    if(sidebar.classList.contains("close")){
-        sidebar.classList.toggle("close")
-        toggleButton.classList.toggle("rotate")
+    submenu.classList.toggle("show");
+    button.classList.toggle("rotate");
+
+    if (sidebar.classList.contains("close")) {
+        sidebar.classList.remove("close");
+        toggleButton.classList.remove("rotate");
     }
 }
-function closeAllSubmenus(){
-    Array.from(sidebar.getElementsByClassName("show")).forEach( ul =>{
-        ul.classList.remove("show")
-        ul.previoussElementSibling.classList.remove("rotate")
-    })
-}
+
+function closeAllSubmenus() {
+    Array.from(sidebar.getElementsByClassName("sub-menu")).forEach((ul) => {
+        ul.classList.remove("show");
+    });
+
+    Array.from(document.getElementsByClassName("arrow-toggle")).forEach((btn) => {
+        btn.classList.remove("rotate");
+    });
+} 
