033add9a2d6b1f933fbc191ad853d0449db5cf91
Thien-An <tnuualuu@gmail.com>
2025-06-30 11:53:32 +1000

    more minor tweeks & main info on main page
    @todo aboutme page next, and socials

--- app.js
+++ app.js
@@ -95,37 +95,55 @@ document.addEventListener("DOMContentLoaded", function () {
     cs: `
       <h3>Computer Science (Advanced)</h3>
       <ul>
-        <li>Python, HTML, CSS, JavaScript, SQL</li>
-        <li>Data Structures & Algorithms</li>
-        <li>AI, Cybersecurity Interests</li>
+        <li>
+        <span style="color: var(--accent-clr)">Python,</span> HTML, CSS, JavaScript, SQL</li>
+        <li>Data Structures & <span style="color: var(--accent-clr)">Algorithms</span></li>
+        <li>Certificate III in <span style="color: var(--accent-clr)">Information, Communications Technology </span> </li>
       </ul>
     `,
+
     commerce: `
       <h3>Commerce (Business Analytics)</h3>
       <ul>
-        <li>Excel, MYOB, </li>
-        <li>Marketing Strategy Simulations</li>
-        <li>Business Intelligence Tools</li>
+        <li>
+        <span style="color: var(--accent-clr)">Excel,</span> MYOB </li>
+        <li><span style="color: var(--accent-clr)">Accounting</span> for Sustainability and Financials</li>
+        <li>Analytics in <span style="color: var(--accent-clr)">Business and Data</span></li>
       </ul>
     `,
+
     tetris: `
       <h3>Tetris</h3>
       <ul>
-        <li>Former top 38 World, Aug 2020 </li>
-        <li>Former No. 2 Australia, Aug 2020 </li>
-        <li>I'm washed </li>
-        <img src="images/tetrisRanking">
+        <li>Former <span style="color: var(--accent-clr)">Top 38</span> Global</li>
+        <li>Former <span style="color: var(--accent-clr)">No 2</span> Australia</li>
+        <li>I'm washed <span style="color: var(--accent-clr)">(Not sped up btw)</span></li>
       </ul>
+      <img src="images/tetrisRanking.jpg" alt="Tetris Ranking" class="tetris-image" />
     `,
+
     aboutMe: `
       <h3>About Me</h3>
       <ul>
         <li>
           Passionate about 
-          <span style="color: var(--accent-clr)">Music, Coding & Games</span>
+          <span style="color: var(--accent-clr)">Classical</span>
+          <span style="color: var(--trippleHover-clr); font-weight: bold">
+            Music
+            <svg class="shake-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="var(--trippleHover-clr)"><path d="M400-120q-66 0-113-47t-47-113q0-66 47-113t113-47q23 0 42.5 5.5T480-418v-422h240v160H560v400q0 66-47 113t-113 47Z"/></svg>, 
+          </span> 
+          <span style="color: var(--accent-clr)">Coding random useless</span>
+          <span style="color: var(--trippleHover-clr); font-weight: bold"> macros
+            <svg class="shake-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="var(--trippleHover-clr)"><path d="m384-336 56-57-87-87 87-87-56-57-144 144 144 144Zm192 0 144-144-144-144-56 57 87 87-87 87 56 57ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z"/></svg>
+          </span>
+          &
+          <span style="color: var(--accent-clr)"> Video </span>
+          <span style="color: var(--trippleHover-clr); font-weight: bold"> 
+            games
+            <svg class="shake-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="var(--trippleHover-clr)"><path d="M182-200q-51 0-79-35.5T82-322l42-300q9-60 53.5-99T282-760h396q60 0 104.5 39t53.5 99l42 300q7 51-21 86.5T778-200q-21 0-39-7.5T706-230l-90-90H344l-90 90q-15 15-33 22.5t-39 7.5Zm16-86 114-114h336l114 114q2 2 16 6 11 0 17.5-6.5T800-304l-44-308q-4-29-26-48.5T678-680H282q-30 0-52 19.5T204-612l-44 308q-2 11 4.5 17.5T182-280q2 0 16-6Zm482-154q17 0 28.5-11.5T720-480q0-17-11.5-28.5T680-520q-17 0-28.5 11.5T640-480q0 17 11.5 28.5T680-440Zm-80-120q17 0 28.5-11.5T640-600q0-17-11.5-28.5T600-640q-17 0-28.5 11.5T560-600q0 17 11.5 28.5T600-560ZM310-440h60v-70h70v-60h-70v-70h-60v70h-70v60h70v70Zm170-40Z"/></svg>
+          </span>
         </li>
       </ul>
-
     `
   };
 
@@ -137,5 +155,23 @@ document.addEventListener("DOMContentLoaded", function () {
         void contentBox.offsetWidth;
         contentBox.style.animation = null;
     });
+  });
+});
+
+document.querySelectorAll('.tab-button').forEach(button => {
+  button.addEventListener('click', () => {
+    const tab = button.getAttribute('data-tab');
+
+    // Update buttons
+    document.querySelectorAll('.tab-button').forEach(btn => {
+      btn.classList.remove('active');
+    });
+    button.classList.add('active');
+
+    // Update tab content
+    document.querySelectorAll('.tab-pane').forEach(pane => {
+      pane.classList.remove('active');
+    });
+    document.getElementById(tab).classList.add('active');
   });
 });
